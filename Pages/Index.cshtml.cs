f9ee588f1f366b7ea41492743e49e11eb491eaee
Shanshan <1896768@cegep-heritage.qc.ca>
2022-04-07 16:08:21 -0400

    add main page login and register

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,8 +2,8 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Goose_Panda_Love_Coffee.Models;
 using Microsoft.EntityFrameworkCore;
-
-
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 public class IndexModel : PageModel
 {
@@ -12,17 +12,40 @@ public class IndexModel : PageModel
     private readonly SchoolDBContext _context;
     [BindProperty]
     public User? UserInfo { get; set; }
+
+    // //public IEnumerable<Class> classes { get; private set}
+
+    public IList<Student> Student { get; set; }
+    public string? UserEmail { get; set; }
+
+    public async Task OnGetAsync()
+    {
+
+        Student = await _context.Student.ToListAsync();
+
+        if (User.Identity != null)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            if (claimsIdentity.IsAuthenticated)
+            {
+                var email = claimsIdentity.FindFirst(ClaimTypes.Email);
+                if (email != null)
+                {
+                    UserEmail = email.Value;
+                }
+            }
+        }
+    }
     public IndexModel(ILogger<IndexModel> logger, SchoolDBContext context)
     {
         _logger = logger;
         _context = context;
     }
-
-    public async Task<IActionResult> OnGetAsync()
-    {
-        UserInfo = await _context.User.FirstOrDefaultAsync();
-        return Page();
-    }
+    // public async Task<IActionResult> OnGetAsync()
+    // {
+    //     UserInfo = await _context.User.FirstOrDefaultAsync();
+    //     return Page();
+    // }
     public async Task<IActionResult> OnPostAsync()
     {
         if (UserInfo != null)
@@ -31,11 +54,9 @@ public class IndexModel : PageModel
             {
                 return Page();
             }
-
             _context.Attach(UserInfo).State = EntityState.Modified;
             await _context.SaveChangesAsync();
         }
-
         _logger.Log(LogLevel.Information, UserInfo.Name);
         return RedirectToPage("../Index");
     }
